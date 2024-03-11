6864669cf641fdc40841db7abd187cf7252ef9f9
Myron R Schippers Jr <dubtwee@gmail.com>
2024-03-10 23:47:32 -0400

    mapping over a list of available locations

--- src/components/Mapper/Mapper.jsx
+++ src/components/Mapper/Mapper.jsx
@@ -3,24 +3,35 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './mapper-styles.css';
 
-const POSITION = [51.505, -0.09];
+import { MAP_CENTER_LAT_LONG, DEFAULT_ZOOM, MOCK_LOCATIONS } from './constants';
 
 function Mapper() {
   return (
     <div>
       <h2>Mapper</h2>
 
-      <MapContainer center={POSITION} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={POSITION}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      {MOCK_LOCATIONS && (
+        <MapContainer
+          center={MAP_CENTER_LAT_LONG}
+          zoom={DEFAULT_ZOOM}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {MOCK_LOCATIONS.map((location) => {
+            return (
+              <Marker key={location.id} position={location.position}>
+                <Popup>
+                  <h4 className="marker-title">{location.name}</h4>
+                  <button className="marker-button">DETAILS</button>
+                </Popup>
+              </Marker>
+            );
+          })}
+        </MapContainer>
+      )}
     </div>
   );
 }
---
+++ src/components/Mapper/constants.ts
@@ -0,0 +1,32 @@
+// Center location of initial map load
+export const MAP_CENTER_LAT_LONG = [27.964157, -82.452606];
+// Larger the number the more zoomed in the initial map is
+export const DEFAULT_ZOOM = 10;
+
+// Position markers displayed on map
+export const MOCK_LOCATIONS = [
+  {
+    id: 1,
+    name: 'Location 1',
+    address: '4810 N Himes Ave, Tampa, FL 33614',
+    position: [27.989771, -82.50135],
+  },
+  {
+    id: 2,
+    name: 'Location 2',
+    address: 'Tampa Riverwalk At Curtis Hixon Waterfront Park, Tampa, FL 33602',
+    position: [27.96409, -82.45748],
+  },
+  {
+    id: 3,
+    name: 'Location 3',
+    address: '7801 N 22nd St, Tampa, FL 33610',
+    position: [28.01938, -82.43623],
+  },
+  {
+    id: 4,
+    name: 'Location 4',
+    address: '5620 W Cypress St, Tampa, FL 33607',
+    position: [27.952419, -82.540154],
+  },
+];
